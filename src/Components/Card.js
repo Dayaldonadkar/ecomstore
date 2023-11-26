0516d61ba2f1a96decb0a11d5f6daff6cf087669
Dayaldonadkar <dayalpython@gmail.com>
2023-11-27 03:15:53 +0530

    price formatted of product in card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 const Card = ({ name, price, image }) => {
-  const options = { maximumFractionDigits: 0 };
-  const formattedPrice = Intl.NumberFormat("en-US", options).format(price);
+  const formattedPrice = Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR",
+    maximumFractionDigits: 2,
+  }).format(price / 100);
   return (
     <div className="bg-white px-5 sm:px-3 pt-5 sm:pt-3 pb-2 sm:pb-1 rounded-lg mx-2">
       <img className="h-44" src={image} alt="" />
       <div className="flex justify-between pt-2">
         <p>{name}</p>
-        <p className="text-[#513BEE]">₹{formattedPrice}</p>
+        <p className="text-[#513BEE]">{formattedPrice}</p>
       </div>
     </div>
   );
