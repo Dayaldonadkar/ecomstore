2ed3006431937fa14a8b8103ffc51831df6572f5
Dayaldonadkar <dayalpython@gmail.com>
2023-11-27 03:03:15 +0530

    product page added

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,15 +1,14 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ name, price, image }) => {
+  const options = { maximumFractionDigits: 0 };
+  const formattedPrice = Intl.NumberFormat("en-US", options).format(price);
   return (
     <div className="bg-white px-5 sm:px-3 pt-5 sm:pt-3 pb-2 sm:pb-1 rounded-lg mx-2">
-      <img
-        src="https://images.pexels.com/photos/1275229/pexels-photo-1275229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        alt=""
-      />
+      <img src={image} alt="" />
       <div className="flex justify-between pt-2">
-        <p>Iphone X</p>
-        <p className="text-[#513BEE]">60,000</p>
+        <p>{name}</p>
+        <p className="text-[#513BEE]">₹{formattedPrice}</p>
       </div>
     </div>
   );
--- src/Components/SecondPart.js
+++ src/Components/SecondPart.js
@@ -2,8 +2,11 @@ import React from "react";
 import GridViewIcon from "@mui/icons-material/GridView";
 import ViewListIcon from "@mui/icons-material/ViewList";
 import Card from "./Card";
+import { useGlobalContext } from "../context/Context";
 
 const SecondPart = () => {
+  const { products } = useGlobalContext();
+  console.log("pro", products);
   return (
     <div className="my-5 sm:my-8">
       <div className="flex justify-between">
@@ -23,14 +26,13 @@ const SecondPart = () => {
       </div>
 
       <div className="py-5 sm:grid sm:grid-cols-2 lg:grid-cols-3">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {products.map((currElem) => {
+          return (
+            <div>
+              <Card {...currElem} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
